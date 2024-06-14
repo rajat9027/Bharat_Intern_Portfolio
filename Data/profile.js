e0b05964a5b326ad0224f2e89b4e51a6407b64bf
rajat9027 <135260301+rajat9027@users.noreply.github.com>
2024-06-14 14:17:42 +0530

    Add files via upload

---
+++ Data/profile.js
@@ -0,0 +1,137 @@
+const profileData = {
+  title: "Resume",
+  name: "Rajat Rajput",
+  sub_title: "Intern @Bharatintern",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: `I am an innovative and passionate developer with a primary interest in <strong>JavaScript</strong>
+     and its applications. Experienced in designing and implementing sophisticated UI/UX with
+      Progressive Web Application standards and application life-cycle management.`,
+    contact: {
+      email: "rajatrajput410@gmail.com",
+      phone: "+91-9027062640",
+      address: "Dehradun, IN"
+    }
+  },
+  links: [
+    //{ title: "Codepen", src: "https://codepen.io/imvpn22" },
+    { title: "Github", src: "https://github.com/rajat9027" },
+    { title: "LinkedIn", src: "https://www.linkedin.com/in/rajat-rajput-4129b6237/" }
+    // { title: 'Twitter', src: 'https://twitter.com/imvpn22' }
+    // { title: 'HackerEarth', src: 'https://www.hackerearth.com/@imvpn22' },
+  ],
+  education: [
+    {
+      alma: "Uttaranchal University, Dehardun",
+      duration: "2021 - 2025",
+      std: "B.Tech. (Computer Science & Engineering)",
+      score: "81.01%(uoto 1st sem)"
+    },
+    
+  ],
+  skills: [
+    {
+      category: "Programming Languages",
+      topics: ["C++", "C", "Html","JavaScript","Css","Java"]
+    },
+
+    {
+      category: "Frameworks / Libraries",
+      topics: [
+        "Amazon Web Services",
+        "Shell",
+        "Linux",
+        "Google Cloud",
+        "DevOps",
+        "Microsoft Azure",
+      ]
+    }
+  ],
+  projects: [
+    {
+      title: "Cloud Based E-commerce Platform",
+      duration: "April-May 2023",
+      link: "https://github.com/rajat9027?tab=projects",
+      desc: `A web app to do list, view and buy/sell products.
+      The app is based on AWS Backed Services & reliable architecture.
+      This project was a part of ou 4th semester Cloud Computing Architecture course.
+      Developed using <strong>AWS , Html</strong>,<strong>CSS</strong> and <strong>JavaScript</strong>
+      with Progressive Web-Application standards including full-responsiveness,
+      offline support, and improved accessibilit.`
+    },
+    {
+      title: "To Do list webapp",
+      duration: "July 2023",
+      link: "https://github.com/rajat9027?tab=projects",
+      desc: `A simple To-do list <strong>web-app for listing all the task to be done before the </strong>
+      completeion date 
+      It also has features to delete the added tasks.
+      Developed using <strong> CSS, javascript</strong>
+      <strong>CSS</strong>, <strong>JavaScript</strong> and <strong>Socket.io</strong>.`
+    }
+  ],
+   experiences: [
+    {
+      organization: "Cloud Counselage Private limited",
+      title: `Cloud Computing Intern`,
+      date: "July-September 2023",
+      details: [
+        `Worked on the development of Cloud Computing Applications with help of 
+        <strong> Amazon Web Services </strong> with <strong> Azure</strong> and <strong>Google Cloud </strong> <strong></strong>
+        `,
+        //`Created a customized Content Management System for MPL's website, to update content and images on the fly for different pages.`
+      ]
+    },
+    {
+      organization: "Oasis-Infobyte",
+      title: `Web Developement & Design Intern`,
+      //desc: `Project Engineer`,
+      date: "July 2023 - August 2023",
+      details: [
+        `Contributed to the development of a <strong> Calculator,
+        To-do list webapp and tribute page </strong> 
+         using <strong>Javascript </strong> with <strong>CSS</strong>,
+        <strong> HTML </strong><strong></strong> with help of Sublime-Text editor & VSCode.`,
+       // `Resolved UI/UX issues by refactoring React components and re-structuring CSS/SCSS of an internal project with the codebase of ReactJS-Redux.`
+      ]
+    },
+    {
+      organization: "CodeClause",
+      title: "Web-Development Intern",
+      desc: `<strong>Web-Dev intern</strong> at  <a target='_blank' rel='noreferrer' href='https://www.venuemonk.com/'>
+      <strong> CodeClause </strong> </a> (<a target='_blank' rel='noreferrer' href='https://drive.google.com/open?id=1ch_9x9jCp_TJwKY43FG-pCr_zV9H2iun'> https://goo.gl/Uz1gaf</a>)`,
+      date: "July 2023",
+      details: [
+        `Designed and developed a <strong> Basic Online Resume & To-Do webapp  </strong> using <strong>HTML CSS</strong> and
+        <strong>Javascript </strong> with Sublime Test-Editor & VSCode.`
+      ]
+    },
+    { 
+      organization: "Brillica Services Pvt Ltd",
+      title: "Full-Stack Developer Intern",
+      desc: `<strong>Full-Stack Developer intern</strong> at  <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/company/brillica-services-pvt-ltd/'>
+      <strong> Brillica Services Pvt Ltd </strong> </a> (<a target='_blank' rel='noreferrer' href='https://drive.google.com/open?id=1ch_9x9jCp_TJwKY43FG-pCr_zV9H2iun'> https://goo.gl/Uz1gaf</a>)`,
+      date: "15th June 2022 - 25th July 2022",
+      details: [
+        `Designed and developed a <strong>Venue Onboarding platform</strong> to streamline and automate
+        the onboarding process of partner venues using <strong>React.js-Redux</strong> frontend and
+        <strong>Node.js-Express, MongoDB</strong> backend.`
+      ]
+    }
+  ],
+  certifications: [
+    {
+      desc: `<strong>Full Stack Web Development</strong> by <strong>Brillica Services</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://www.linkedin.com/posts/rajat-rajput-4129b6237_brillicaservices-social-internship-activity-6981978282615296000-ku7l?utm_source=share&utm_medium=member_desktop'>https://goo.gl/RGRzVn</a>)`,
+      date: "june-july 2022"
+    },
+    {
+      desc: `<strong>Web Developement Intern @CodeClause/strong>, <em> 
+      </em>.  <strong>  </strong> 
+       (<a target='_blank' rel='noreferrer' href='https://www.linkedin.com/posts/rajat-rajput-4129b6237_offer-letter-activity-7081997511652966400-bMbn?utm_source=share&utm_medium=member_desktop'>
+       https://goo.gl/X3HEdR</a>)`,
+      date: "july 2023"
+    }
+  ],
+  events: []
+};
